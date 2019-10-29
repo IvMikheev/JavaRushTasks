dbe5998ea349cafe91f3653a73bcad4a53d4a731
Ivan <mikheev.ivan91@gmail.com>
2019-10-29 11:29:56 +0300

    task 2910 completed

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2910/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task29.task2910;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        printDelimiter();
+        game.startCompetition();
+        System.out.println("\n\nВСЕ РЕЗУЛЬТАТЫ:");
+        game.printAllResults();
+        printDelimiter();
+        System.out.println("\n\nСПОРТСМЕНЫ С ТАКИМ ЖЕ ИТОГОВЫМ ВРЕМЕНЕМ, КАК У КОНТРОЛЬНОГО СПОРТСМЕНА:");
+        game.printSportsmanWithEqualTime();
+        printDelimiter();
+    }
+
+    private static void printDelimiter() {
+        System.out.println("=================================================================================================");
+    }
+}
