956df268a04feebe03dbd664fa9b86878ae9fbf8
Ivan <mikheev.ivan91@gmail.com>
2019-11-13 20:23:16 +0300

    task 2712 in progress

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,10 +1,14 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 public class Restaurant {
 
     public static void main(String[] args) {
         Tablet tablet = new Tablet(5);
-        for (int i = 0; i < 4; i++) {
+        Cook cook = new Cook("Amigo");
+        tablet.addObserver(cook);
+        for (int i = 0; i < 1; i++) {
             tablet.createOrder();
         }
     }
