598ec9c4b3ac2ccad17fc76dc5698895aee5845d
Ivan <mikheev.ivan91@gmail.com>
2019-11-18 01:35:29 +0300

    task 3502 completed

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3502/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task35.task3502;
+
+import java.util.List;
+
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static class SomeClass<T extends Number> {
+    }
+
+    public static void main(String[] args) {
+    }
+}
