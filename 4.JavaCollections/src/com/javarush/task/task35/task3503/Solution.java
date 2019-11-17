11962bf11d75308695b9397fc4167e32fbffef49
Ivan <mikheev.ivan91@gmail.com>
2019-11-18 01:54:51 +0300

    task 3503 completed

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3503/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task35.task3503;
+
+public class Solution<T extends ClassForGenerics & InterfaceForGenerics> {
+    public static void main(String[] args) {
+        Solution<TestClassGood> testClassSolution = new Solution<>();
+        testClassSolution.check();
+    }
+
+    public void check() {
+        System.out.println("Works!");
+    }
+
+    public static class TestClassGood extends ClassForGenerics implements InterfaceForGenerics {
+    }
+
+    public static class TestClassWrong1 extends ClassForGenerics {
+    }
+
+    public static class TestClassWrong2 implements InterfaceForGenerics {
+    }
+}
