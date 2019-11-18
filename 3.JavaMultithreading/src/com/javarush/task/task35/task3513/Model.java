e8871db534cfbfdeb2080b0955ce35c612ccf1d2
Ivan <mikheev.ivan91@gmail.com>
2019-11-18 22:59:14 +0300

    task 3513 in progress

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,4 +1,15 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int x = 0; x < gameTiles.length; x++) {
+            for (int y = 0; y < gameTiles.length; y++) {
+                gameTiles[y][x] = new Tile();
+            }
+        }
+    }
 }
