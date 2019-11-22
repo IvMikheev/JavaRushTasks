b03788bc77192fc57610bdd3ebd8d1ea01327ab5
Ivan <mikheev.ivan91@gmail.com>
2019-11-22 20:07:28 +0300

    task 2213 in progress

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
@@ -2,7 +2,34 @@ package com.javarush.task.task22.task2213;
 
 public class FigureFactory {
 
-    public static Figure createRandomFigure(int x,int y) {
-        return null;
+    public static final int[][][] BRICKS = {{
+            {1, 1, 0},                          //   X X
+            {0, 1, 1},                          //     X X
+            {0, 0, 0}}, {                       //
+
+            {1, 0, 0},                          //   X
+            {1, 1, 0},                          //   X X
+            {0, 1, 0}}, {                       //     X
+
+            {0, 1, 0},                          //   X
+            {0, 1, 0},                          //   X
+            {0, 1, 0}}, {                       //   X
+
+            {1, 1, 0},                          //   X X
+            {1, 1, 0},                          //   X X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {0, 1, 0},                          //     X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {1, 1, 1},                          //   X X X
+            {0, 0, 0}}                          //
+    };
+
+    public static Figure createRandomFigure(int x, int y) {
+        int index = (int) (Math.random() * 6);
+        return new Figure(x, y, BRICKS[index]);
     }
 }
