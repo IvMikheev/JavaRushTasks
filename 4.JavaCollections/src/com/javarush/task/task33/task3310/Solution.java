ffdf061fd06d2b0ad3a6767d9e286ac1e369effc
Ivan <mikheev.ivan91@gmail.com>
2019-11-29 16:18:26 +0300

    task 3310 in progress

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -1,8 +1,55 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.HashMapStorageStrategy;
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
 
     public static void main(String[] args) {
+        testStrategy(new HashMapStorageStrategy(), 10000);
+    }
 
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings) {
+        Set<Long> ids = new HashSet<>();
+        for (String val : strings) {
+            ids.add(shortener.getId(val));
+        }
+        return ids;
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys) {
+        Set<String> strings = new HashSet<>();
+        for (long id : keys) {
+            strings.add(shortener.getString(id));
+        }
+        return strings;
+    }
+
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber) {
+        Helper.printMessage(strategy.getClass().getSimpleName());
+        Set<String> randomStrings = new HashSet<>();
+        for (int i = 0; i < elementsNumber; i++) {
+            randomStrings.add(Helper.generateRandomString());
+        }
+
+        Shortener shortener = new Shortener(strategy);
+        long startMs = new Date().getTime();
+        Set<Long> ids = getIds(shortener, randomStrings);
+        long resultMs = new Date().getTime() - startMs;
+        Helper.printMessage("Result of getIds() method: " + resultMs + "ms.");
+
+        startMs = new Date().getTime();
+        Set<String> strings = getStrings(shortener, ids);
+        resultMs = new Date().getTime() - startMs;
+        Helper.printMessage("Result of getStrings() method: " + resultMs + "ms.");
+
+        boolean equals = randomStrings.containsAll(strings);
+
+        if (equals) Helper.printMessage("Тест пройден.");
+        else Helper.printMessage("Тест не пройден.");
     }
 }
