c8f6d086312f1de4dbf7fb0046079e9432b9670e
Ivan <mikheev.ivan91@gmail.com>
2019-12-04 21:58:18 +0300

    task 3802 completed

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task38.task3802;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws Exception {
+        InputStream is = new FileInputStream("");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
