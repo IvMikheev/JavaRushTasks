94bc8c531d313e114e62b22745b348a28118345a
Ivan <mikheev.ivan91@gmail.com>
2019-12-08 16:46:48 +0300

    task 4002 completed

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4002/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task40.task4002;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.sendPost("https://enbhq7zoop70j.x.pipedream.net/", "name=zapp&mood=good&locale=&id=777");
+    }
+
+    public void sendPost(String url, String urlParameters) throws Exception {
+        HttpClient client = getHttpClient();
+        HttpPost request = new HttpPost(url);
+
+        request.addHeader("User-Agent", "Mozilla/5.0");
+
+        List<NameValuePair> valuePairs = new ArrayList<>();
+        String[] strings = urlParameters.split("&");
+
+        for (String s : strings) {
+            valuePairs.add(new BasicNameValuePair(s.substring(0, s.indexOf("=")), s.substring(s.indexOf("=") + 1)));
+        }
+
+        request.setEntity(new UrlEncodedFormEntity(valuePairs));
+
+        HttpResponse response = client.execute(request);
+
+        System.out.println("Response Code: " + response.getStatusLine().getStatusCode());
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String responseLine;
+        while ((responseLine = bufferedReader.readLine()) != null) {
+            result.append(responseLine);
+        }
+
+        System.out.println("Response: " + result.toString());
+    }
+
+    protected HttpClient getHttpClient() {
+        return HttpClientBuilder.create().build();
+    }
+}
