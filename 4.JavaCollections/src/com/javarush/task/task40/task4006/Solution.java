298db656ae729c0dd8a50c05d759c70e44d283e8
Ivan <mikheev.ivan91@gmail.com>
2019-12-09 20:04:53 +0300

    task 4006 completed

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task40.task4006;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        getSite(new URL("http://javarush.ru/social.html"));
+    }
+
+    public static void getSite(URL url) {
+        try {
+            Socket socket = new Socket(url.getHost(), url.getDefaultPort());
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            writer.println("GET "+url.getFile());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String responseLine;
+
+            while ((responseLine = bufferedReader.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+            bufferedReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
